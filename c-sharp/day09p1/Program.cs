f4991705ee897e90a1defd14b5c4a82361a81537
Andres Dejesus <andresdejesus123@gmail.com>
2024-01-07 23:42:02 -0600

    day 9

---
+++ c-sharp/day09p1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+
+class Knot
+{
+    public Knot()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public bool Touching(Knot other) => Math.Abs(x - other.x) < 2 && Math.Abs(y - other.y) < 2;
+}
+
+class Program
+{
+    static void Step(char dir, Knot head, Knot tail, HashSet<(int, int)> positions)
+    {
+        switch (dir)
+        {
+            case 'U':
+                {
+                    head.y--;
+                    if (!head.Touching(tail))
+                    {
+                        tail.x = head.x;
+                        tail.y = head.y + 1;
+                    }
+                    break;
+                }
+
+            case 'D':
+                {
+                    head.y++;
+                    if (!head.Touching(tail))
+                    {
+                        tail.x = head.x;
+                        tail.y = head.y - 1;
+                    }
+                    break;
+                }
+            case 'L':
+                {
+                    head.x--;
+                    if (!head.Touching(tail))
+                    {
+                        tail.x = head.x + 1;
+                        tail.y = head.y;
+                    }
+                    break;
+                }
+            case 'R':
+                {
+                    head.x++;
+                    if (!head.Touching(tail))
+                    {
+                        tail.x = head.x - 1;
+                        tail.y = head.y;
+                    }
+                    break;
+                }
+        }
+
+        positions.Add((tail.x, tail.y));
+    }
+
+    static void Main()
+    {
+        var time = new Stopwatch();
+        time.Start();
+
+        var positions = new HashSet<(int, int)>();
+        positions.Add((0, 0));
+        var (head, tail) = (new Knot(), new Knot());
+        foreach (var line in File.ReadLines("input"))
+        {
+            var split = line.Split(' ');
+            char dir = split[0][0];
+            int steps = Int32.Parse(split[1]);
+            foreach (var _ in Enumerable.Range(0, steps))
+            {
+                Step(dir, head, tail, positions);
+            }
+        }
+
+        Console.WriteLine($"positions visited: {positions.Count()}");
+
+        time.Stop();
+        Console.WriteLine($"elapsed: {time.Elapsed.TotalMilliseconds} ms");
+    }
+}
---
+++ c-sharp/day09p2/Program.cs
@@ -0,0 +1,158 @@
+﻿using System.Diagnostics;
+
+class Knot
+{
+    private Knot? _next;
+
+    public Knot Next => _next ?? throw new Exception("don't access Next on head");
+
+    public Knot(Knot? next)
+    {
+        _next = next;
+        x = 0;
+        y = 0;
+    }
+
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public void FollowNext()
+    {
+        if (Next.y - y == -2)
+        {
+            y--;
+            if (Next.x - x == 2)
+            {
+                x++;
+            }
+            else if (Next.x - x == -2)
+            {
+                x--;
+            }
+            else
+            {
+                x = Next.x;
+            }
+        }
+        if (Next.y - y == 2)
+        {
+            y++;
+            if (Next.x - x == 2)
+            {
+                x++;
+            }
+            else if (Next.x - x == -2)
+            {
+                x--;
+            }
+            else
+            {
+                x = Next.x;
+            }
+        }
+        if (Next.x - x == -2)
+        {
+            x--;
+            if (Next.y - y == 2)
+            {
+                y++;
+            }
+            else if (Next.y - y == -2)
+            {
+                y--;
+            }
+            else
+            {
+                y = Next.y;
+            }
+        }
+        if (Next.x - x == 2)
+        {
+            x++;
+            if (Next.y - y == 2)
+            {
+                y++;
+            }
+            else if (Next.y - y == -2)
+            {
+                y--;
+            }
+            else
+            {
+                y = Next.y;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Step(char dir, Knot head, Knot[] tail)
+    {
+        switch (dir)
+        {
+            case 'U':
+                {
+                    head.y--;
+                    break;
+                }
+
+            case 'D':
+                {
+                    head.y++;
+                    break;
+                }
+            case 'L':
+                {
+                    head.x--;
+                    break;
+                }
+            case 'R':
+                {
+                    head.x++;
+                    break;
+                }
+        }
+
+        foreach (var knot in tail)
+        {
+            knot.FollowNext();
+        }
+    }
+
+    static void Main()
+    {
+        var time = new Stopwatch();
+        time.Start();
+
+        var positions = new HashSet<(int, int)>();
+        positions.Add((0, 0));
+
+        var head = new Knot(null);
+        var next = head;
+        var tail = new Knot[9];
+        for (var i = 0; i < 9; i++)
+        {
+            tail[i] = new Knot(next);
+            next = tail[i];
+        }
+
+        foreach (var line in File.ReadLines("input"))
+        {
+            var split = line.Split(' ');
+            var dir = split[0][0];
+            var steps = Int32.Parse(split[1]);
+
+            foreach (var _ in Enumerable.Range(0, steps))
+            {
+                Step(dir, head, tail);
+                positions.Add((tail[8].x, tail[8].y));
+            }
+        }
+
+        Console.WriteLine($"positions visited: {positions.Count()}");
+
+        time.Stop();
+        Console.WriteLine($"elapsed: {time.Elapsed.TotalMilliseconds} ms");
+    }
+}
