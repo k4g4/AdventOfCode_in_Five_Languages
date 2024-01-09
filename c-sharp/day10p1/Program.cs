7b9c80bda5cf7d9a2654c74848904c6ba97484a1
Andres Dejesus <andresdejesus123@gmail.com>
2024-01-08 20:22:21 -0600

    day 10

---
+++ c-sharp/day10p1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        var time = new Stopwatch();
+        time.Start();
+
+        var instructions = new List<int>();
+
+        foreach (var line in File.ReadLines("input"))
+        {
+            if (line == "noop")
+            {
+                instructions.Add(0);
+            }
+            else
+            {
+                instructions.Add(0);
+                instructions.Add(Int32.Parse(line.Split(' ')[1]));
+            }
+        }
+
+        var (i, register, total) = (0, 1, 0);
+        foreach (var cycle in new int[] { 19, 40, 40, 40, 40, 40 })
+        {
+            foreach (var _ in Enumerable.Range(0, cycle))
+            {
+                register += instructions[i++];
+            }
+
+            total += (i + 1) * register;
+        }
+
+        Console.WriteLine($"total: {total}");
+
+        time.Stop();
+        Console.WriteLine($"elapsed: {time.Elapsed.TotalMilliseconds} ms");
+    }
+}
---
+++ c-sharp/day10p2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        var time = new Stopwatch();
+        time.Start();
+
+        var instructions = new List<int>();
+
+        foreach (var line in File.ReadLines("input"))
+        {
+            if (line == "noop")
+            {
+                instructions.Add(0);
+            }
+            else
+            {
+                instructions.Add(0);
+                instructions.Add(Int32.Parse(line.Split(' ')[1]));
+            }
+        }
+
+        var (i, register, total) = (0, 1, 0);
+        foreach (var cycle in new int[] { 40, 40, 40, 40, 40, 40 })
+        {
+            foreach (var _ in Enumerable.Range(0, cycle))
+            {
+                if (i % 40 >= register - 1 && i % 40 <= register + 1)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+                register += instructions[i++];
+            }
+            Console.WriteLine();
+        }
+
+        time.Stop();
+        Console.WriteLine($"elapsed: {time.Elapsed.TotalMilliseconds} ms");
+    }
+}
