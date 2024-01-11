5a68335e0c3b76f0a2ec3185550d49680efb921b
Andres Dejesus <andresdejesus123@gmail.com>
2024-01-11 10:08:44 -0600

    recursive strategy

--- c-plus-plus/day16p1/main.cpp
+++ c-plus-plus/day16p1/main.cpp
@@ -2,9 +2,10 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <regex>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #define INPUT "example"
@@ -16,6 +17,13 @@ struct Name : array<char, 2> {
     Name(const smatch& match) {
         (*this)[0] = *match[0].first;
         (*this)[1] = *(match[0].first + 1);
+    }
+};
+
+template <>
+struct std::hash<Name> {
+    size_t operator()(const Name& name) const noexcept {
+        return hash<char>{}(name[0]) ^ (hash<char>{}(name[1]));
     }
 };
 
@@ -36,6 +44,12 @@ ostream& operator<<(ostream& os, const Valve& valve) {
     return os << "'";
 }
 
+uint recurse(const unordered_map<Name, Valve>& valves, Name currentValve,
+             unordered_set<Name> released, uint currentFlow, size_t currentHash,
+             unordered_map<size_t, uint> memo) {
+    //
+}
+
 int main() {
     auto time = steady_clock::now();
 
@@ -46,13 +60,13 @@ int main() {
     const regex flowRatePattern{"\\d+"};
     smatch match;
 
-    map<Name, Valve> valves;
+    unordered_map<Name, Valve> valves;
 
     while (getline(input, line)) {
         Valve valve;
 
         regex_search(line, match, namePattern);
-        Name name(match);
+        Name name{match};
 
         for (auto remainder = match.suffix().first;
              regex_search(remainder, line.cend(), match, namePattern);
@@ -70,6 +84,10 @@ int main() {
         cout << "name: " << nameAndValve.first << "\nvalve:\n"
              << nameAndValve.second << '\n';
     }
+
+    unordered_set<Name> released;
+    unordered_map<size_t, uint> memo;
+    uint flow = 0;
 
     cout << "elapsed: "
          << duration_cast<duration<double, milli>>(steady_clock::now() - time)
